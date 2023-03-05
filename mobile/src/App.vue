3b7cecd1b95968e06cd20e5bc40ddcd2efdfe187
papll_co <109155319+xioamang@users.noreply.github.com>
2023-03-05 20:52:57 +0800

    脚手架

---
+++ mobile/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HomePage></HomePage>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+div{
+  margin: 0 auto;
+  width: 100%;
+}
+</style>
